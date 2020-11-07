fb9a92bd7f8fee28b8e7d730b839e528a81fc002
Anstonen <anssi.lehtonen1@gmail.com>
2020-11-07 17:57:46 +0200

    Added foreign key (question id) to answer model

--- mood/models.py
+++ mood/models.py
@@ -9,13 +9,14 @@ class Mood(models.Model):
     user = models.ForeignKey(Person, on_delete=models.PROTECT, default=None)
     date = models.DateTimeField(default=None)
     time = models.IntegerField(default=None, choices = [(i,i) for i in range(3)])
-    value = models.IntegerField(choices = [(i,i) for i in range(6)], default=None)
-    
-class Answers(models.Model):
-    user = models.ForeignKey(Person, on_delete=models.PROTECT, default=None)
-    answer = models.IntegerField(choices = [(i,i) for i in range(2)], default=None)
+    value = models.IntegerField(choices = [(i,i) for i in range(6)], default=None)  
 
 class Questions(models.Model):
     influence = models.IntegerField(default=None)
     time = models.IntegerField(choices = [(i,i) for i in range(3)], default=None)
     question_text = models.CharField(max_length=255, default=None)
+
+class Answers(models.Model):
+    user = models.ForeignKey(Person, on_delete=models.PROTECT, default=None)
+    question = models.ForeignKey(Questions, on_delete=models.PROTECT, default=None)
+    answer = models.IntegerField(choices = [(i,i) for i in range(2)], default=None)
