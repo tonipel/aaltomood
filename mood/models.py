591e13ae63eb4ee7f9ddbab301233b6d689131eb
rosampenttinen <rosapenttinen@Rosa-MacBook-Air.local>
2020-11-07 15:35:24 +0200

    added models.

--- mood/models.py
+++ mood/models.py
@@ -1,5 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    wellbeing = models.IntegerField()
+
 class Mood(models.Model):
-    mood_text = models.CharField(max_length=30)
+    user = models.ForeignKey(Person, on_delete=models.PROTECT,default=None)
+    date = models.DateTimeField(default=None)
+    time = models.IntegerField(choices = [(i,i) for i in range(3)])
+    value = models.IntegerField(choices = [(i,i) for i in range(6)],default=None)
+    
+class Answers(models.Model):
+    user = models.ForeignKey(Person, on_delete=models.PROTECT,default=None)
+    answer = models.IntegerField(choices = [(i,i) for i in range(2)])
+
+class Questions(models.Model):
+    influence = models.IntegerField()
+    time = models.IntegerField(choices = [(i,i) for i in range(3)])
