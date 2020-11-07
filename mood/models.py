503e5b6d6740f8cd19ee2460fa5d8f393aa08b86
Toni Pellinen <toni.pellinen@hotmail.com>
2020-11-07 15:40:12 +0200

    Adjust models, add migrations

--- mood/models.py
+++ mood/models.py
@@ -6,15 +6,15 @@ class Person(models.Model):
     wellbeing = models.IntegerField()
 
 class Mood(models.Model):
-    user = models.ForeignKey(Person, on_delete=models.PROTECT,default=None)
+    user = models.ForeignKey(Person, on_delete=models.PROTECT, default=None)
     date = models.DateTimeField(default=None)
-    time = models.IntegerField(choices = [(i,i) for i in range(3)])
-    value = models.IntegerField(choices = [(i,i) for i in range(6)],default=None)
+    time = models.IntegerField(default=None, choices = [(i,i) for i in range(3)])
+    value = models.IntegerField(choices = [(i,i) for i in range(6)], default=None)
     
 class Answers(models.Model):
-    user = models.ForeignKey(Person, on_delete=models.PROTECT,default=None)
-    answer = models.IntegerField(choices = [(i,i) for i in range(2)])
+    user = models.ForeignKey(Person, on_delete=models.PROTECT, default=None)
+    answer = models.IntegerField(choices = [(i,i) for i in range(2)], default=None)
 
 class Questions(models.Model):
-    influence = models.IntegerField()
-    time = models.IntegerField(choices = [(i,i) for i in range(3)])
+    influence = models.IntegerField(default=None)
+    time = models.IntegerField(choices = [(i,i) for i in range(3)], default=None)
