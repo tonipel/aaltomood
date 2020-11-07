2fa8c4456b155a7a11ee0fd4a61879b77141ab49
Toni Pellinen <toni.pellinen@hotmail.com>
2020-11-07 16:02:30 +0200

    Admin site registrations

--- mood/admin.py
+++ mood/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from mood.models import Person, Mood, Answers, Questions
 
-# Register your models here.
+admin.site.register(Person)
+admin.site.register(Mood)
+admin.site.register(Answers)
+admin.site.register(Questions)
