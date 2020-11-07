c9d45ecb6922d6618e926eaa5bd300d2e4ba631f
Rosa Penttinen <rosapenttinen@Rosa-MacBook-Air.local>
2020-11-07 13:34:41 +0200

    added home page

--- mood/urls.py
+++ mood/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from mood.views import MoodListView
+from mood.views import HomePage
 
 urlpatterns = [
     path('moods', MoodListView.as_view(), name='index'),
+    path('home', HomePage.as_view(), name='home')
 ]
